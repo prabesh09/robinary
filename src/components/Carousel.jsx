420f57d6f0e78aaac19e7bc54082b4fe02e02839
Prabesh Shrestha <prabeshshrestha90@gmail.com>
2024-06-21 16:54:27 +0545

    Updating the carousel 🖼️

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,28 @@
+//import React from 'react'
+
+import { useEffect } from "react";
+import { useState } from "react"
+
+const Carousel = () => {
+    const carousel = ["banner", "banner2", "banner3"]
+    const [current, setCurrent] = useState(0);
+
+    const handleChange = () => {
+        setCurrent(current < 2 ? current + 1 : 0)
+    }
+
+    useEffect(() => {
+        const timeout = setTimeout(handleChange, 2000)
+        return () => clearTimeout(timeout)
+    }, [current])
+
+    return (
+        <div className="w-full flex justify-center items-center gap-2">
+            <img src={`../public/${carousel[current]}.png`} alt="" className="h-full" />
+            {/* <button onClick={handleChange} className="bg-red-500 text-white px-5 rounded-2xl">
+                NEXT
+            </button> */}
+        </div >)
+}
+
+export default Carousel
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,18 @@
 // import React from 'react'
 import Categories from "./Categories"
+import Carousel from "./Carousel"
 
 const Hero = () => {
+
     return (
         <div className="h-[50vh] py-10 flex items-top px-10 justify-between">
             <Categories />
-            <div className="w-full flex justify-center">
-                <img src="../public/banner.png" alt="" className="h-full" />
-            </div>
+            <Carousel />
+
+
+            {/* <div className="w-[75em]">
+                product cards
+            </div> */}
         </div>
     )
 }
